e8ae2b54fbdb687a1f100e7338c9b57386ca2fa2
Yang <zhy0216@gmail.com>
2023-01-06 21:34:43 +0800

    updated

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # json-template
-json template
+in some cases, we will write a lot of json files. They may have lots of similar part, so i come up an idea
+to use json-template reduce the unnecessary repetition. i extend json language with two simple concepts.
+
+
+## $extend
+
+## pipe with jmespath
+
+
 
 
 * https://jmespath.org/
--- src/parse.ts
+++ src/parse.ts
@@ -1,39 +1,38 @@
-
-type JSONValue =
-    | string
-    | number
-    | boolean
-    | null
-    | JSONObject
-    | JSONValue[];
+type JSONValue = string | number | boolean | null | JSONObject | JSONValue[];
 
 interface JSONObject {
-    [x: string]: JSONValue;
+  [x: string]: JSONValue;
 }
 
-type ParseContext = Record<string, JSONValue | undefined>
+type ParseContext = Record<string, JSONValue | undefined>;
 
-const isObject = (data: any): data is JSONObject => data !== null && typeof data === 'object'
+const isObject = (data: any): data is JSONObject =>
+  data !== null && typeof data === "object";
 
 const parse = (data: JSONValue, context: ParseContext): JSONValue => {
-  if(Array.isArray(data)) {
-    return data.map(v => parse(data, context))
-  } else if(isObject(data)) {
-    return parseObject(data, context)
+  if (Array.isArray(data)) {
+    return data.map((v) => parse(data, context));
+  } else if (isObject(data)) {
+    return parseObject(data, context);
   } else {
-    return data
+    return data;
   }
-}
+};
 
 const parseObject = (data: JSONObject, context: ParseContext): JSONObject => {
-  const {$extend, ...rest} = data
-  const extendedJson = typeof $extend === 'string'? context[$extend]: undefined
-  if(!isObject(extendedJson)) {
-    throw new Error(`wrong with extend: ${$extend}`)
+  const { $extend, ...rest } = data;
+
+  // jmespath here
+  const extendedJson =
+    typeof $extend === "string" ? context[$extend] : undefined;
+  if (!isObject(extendedJson)) {
+    throw new Error(`wrong with extend: ${$extend}`);
   }
 
-  
+  const parsedJson = Object.keys(rest).reduce(
+    (prev, curr) => ({ ...prev, [curr]: parse(rest[curr], context) }),
+    {}
+  );
 
-
-  return {...extendedJson, ...rest}
-}
+  return { ...extendedJson, ...parsedJson };
+};
